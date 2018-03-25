74b9102f0037a55d5f20aea161051b0439885b48
Daniel Aragão Abreu Filho <danielfilhoce@gmail.com>
2018-03-25 17:13:51 -0300

    utf 8

--- noticias.js
+++ noticias.js
@@ -13,7 +13,7 @@ var server = http.createServer(function(request, response){
     }else if(categoria == "/beleza"){
         response.end("<html><body>Notícias de beleza</body></html>")
     }else{
-        response.end("<html><body>Portal de notícias</body></html>")
+        response.end("<html><head><meta charset='utf-8'></head><body>Portal de notícias</body></html>")
     }
 });
 
